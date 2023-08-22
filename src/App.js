100ea6fe3acb9195afb1533c59d96cb31c636185
algoristy <princechauhan060101@gmail.com>
2023-08-22 10:42:05 +0530

    Update all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import IntuitionContainer from "./components/IntuitionContainer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container mb-5">
+        <p className="title pb-4">Blaze-Intuition</p>
+        <div class="row">
+          
+          <p class="description pb-3">
+            Intuition is an AI game developed using Minimax algorithm with
+            Alpha-beta pruning. This game is played between two players and
+            winners need to be the last player with an available move.
+          </p>
+          <p class="description pb-5">
+            <b>How to play it?</b> <br /> <br />
+            You are the player 1 (the blue one) and AI will be the player 2 (the
+            yellow one), start by any cell and in each play you are allowed to
+            visit any adjacent unvisited cell.
+            <br />
+            <br />
+            Cells visited by player 1 will be marked by faint grey shade.
+            <br />
+            Cells visited by player 2 will be marked by darker grey shade.
+          </p>
+          
+          <IntuitionContainer width="3" height="3"></IntuitionContainer>
+          
+        </div>
+      </div>
     </div>
   );
 }
